c97c0ff537f2528233428bc9a6288e8797923e1b
Pedro Matias Menezes <pedromm2406@gmail.com>
2023-03-30 22:59:10 -0300

    feat: Helmet

--- src/config/customExpress.ts
+++ src/config/customExpress.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import fileUpload from "express-fileupload";
+import helmet from "helmet";
 import apiRules from "../middlewares/apiRules";
 import requestLogger from "../middlewares/requestLogger";
 import router from "../routes/router";
-import fileUpload from "express-fileupload";
 
 export default () => {
   const app = express();
+  app.use(helmet());
   app.use(requestLogger);
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
